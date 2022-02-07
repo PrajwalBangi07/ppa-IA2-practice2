57308d1b5d87a935207a9348ed21d67a3c34e15b
PrajwalBangi07 <97020307+PrajwalBangi07@users.noreply.github.com>
2022-02-07 10:35:39 +0000

    p1 abc

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
-
+#include<stdio.h>
+void input(float *b, float *h)
+{
+ 
+  printf("enter the values of b and h");
+  scanf("%f%f",b,h);
 }
+
+void find_area(float b,float h,float *area)
+
+  
+{
+  *area=((b*h)/2);
+}
+
+void output(float b,float h,float area)
+{
+  printf("The area is %f*%f is %f",b,h,area);
+}
+int main()
+{
+  float b,h,area;
+ input(&b,&h);
+ find_area(b,h,&area);
+  output(b,h,area);
+  return 0;
+}
+
---
+++ p2original.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int input_side()
+{
+  int a,b,c
+  printf("Enter 3 numbers");
+  scanf("%d%d%d",&a,&b,&c);
+  return a,b,c;
+}
+int check_scalene(int a,int b,int c)
+{
+  If (a!=b && b!=c && c!=a);
+  isscalene=1;
+return a,b,c;
+}
+void output(int a,int b,int c)
