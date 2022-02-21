c814c673078758810da32f1e717c3623cabbed28
PrajwalBangi07 <97020307+PrajwalBangi07@users.noreply.github.com>
2022-02-21 10:23:40 +0000

    p2.p3.p4

--- p2original.c
+++ p2original.c
@@ -1,15 +1,45 @@
 #include<stdio.h>
 int input_side()
 {
-  int a,b,c
-  printf("Enter 3 numbers");
-  scanf("%d%d%d",&a,&b,&c);
-  return a,b,c;
+  int a;
+  printf("enter the side of a triangle");
+  scanf("%d",&a);
+  return a;
 }
-int check_scalene(int a,int b,int c)
+int check_scalene(int a, int b, int c)
 {
-  If (a!=b && b!=c && c!=a);
-  isscalene=1;
-return a,b,c;
+  int result;
+  if( a!=b && b!=c && c!=a)
+  {
+     
+      result=1;
+  }
+  else
+  {
+   result=0 ;
+  }
+  return result;
+
 }
-void output(int a,int b,int c)
+void output(int a,int b, int c, int isscalene)
+{ 
+int result;
+  if(result == 1)
+  {
+    printf("it is a scalene triangle");
+
+  }
+  if(result == 0){
+    printf("not a scalene triangle");
+  }}
+  int main()
+  {
+    int a,b,c,result;
+    a = input_side();
+    b = input_side();
+    c = input_side();
+    result =check_scalene(a,b,c);
+    output(a,b,c,result);
+    return 0;
+
+  }
